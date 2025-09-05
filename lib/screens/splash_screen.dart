0de6cecad70e178ddaf3f89b6723fb48f2017f60
Bishal Chandro Modak <bishalchandromodak@gmail.com>
2025-09-05 23:15:49 +0600

    splash screen ui fixed

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mediconnect/screens/Sign_in_screen.dart';
 import 'package:mediconnect/screens/user_home_screen.dart';
-
 import 'call_ambulance/Assistance/assistent_method.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,47 +12,12 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with TickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  late AnimationController _logoController;
-  late AnimationController _textController;
-  late Animation<double> _logoAnimation;
-  late Animation<Offset> _textAnimation;
 
   @override
   void initState() {
     super.initState();
-
-    // Logo animation (fade + scale)
-    _logoController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-    _logoAnimation = CurvedAnimation(
-      parent: _logoController,
-      curve: Curves.easeOut,
-    );
-
-    _textController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1500),
-    );
-    _textAnimation =
-        Tween<Offset>(
-          begin: const Offset(0, 1), // starts below screen
-          end: Offset.zero,
-        ).animate(
-          CurvedAnimation(parent: _textController, curve: Curves.easeOutCubic),
-        );
-
-    _logoController.forward();
-    _textController.forward();
-
-    startTimer();
-  }
-
-  startTimer() {
     Timer(const Duration(seconds: 3), () async {
       if (firebaseAuth.currentUser != null) {
         await AssistentMethods.readCurrentOnlineUserInfo();
@@ -71,13 +35,6 @@ class _SplashScreenState extends State<SplashScreen>
   }
 
   @override
-  void dispose() {
-    _logoController.dispose();
-    _textController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal.shade50,
@@ -85,48 +42,36 @@ class _SplashScreenState extends State<SplashScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ScaleTransition(
-              scale: _logoAnimation,
-              child: FadeTransition(
-                opacity: _logoAnimation,
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.teal.shade100,
-                  ),
-                  child: const Icon(
-                    Icons.local_hospital,
-                    size: 80,
-                    color: Colors.teal,
-                  ),
-                ),
+            Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.teal.shade100,
+              ),
+              child: const Icon(
+                Icons.local_hospital,
+                size: 80,
+                color: Colors.teal,
               ),
             ),
-            const SizedBox(height: 30),
-            SlideTransition(
-              position: _textAnimation,
-              child: Column(
-                children: [
-                  Text(
-                    "MediConnect",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.teal.shade800,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Connecting You to Better Healthcare",
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                  ),
-                ],
+            const SizedBox(height: 20),
+            // App Name
+            Text(
+              "MediConnect",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal.shade800,
               ),
             ),
-            const SizedBox(height: 50),
-            const CircularProgressIndicator(color: Colors.teal, strokeWidth: 3),
+            const SizedBox(height: 10),
+            const Text(
+              "Connecting You to Better Healthcare",
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+            const SizedBox(height: 40),
+            const CircularProgressIndicator(color: Colors.teal),
           ],
         ),
       ),
